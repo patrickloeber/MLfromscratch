94bded502bf46dfb39123f110f3eb2c81141a2d7
Python Engineer <pythonengineerman@gmail.com>
2019-09-02 19:11:56 +0200

    Updated KNN and added tests

--- mla/knn.py
+++ mla/knn.py
@@ -3,40 +3,29 @@ from collections import Counter
 
 
 def euclidean_distance(x1, x2):
-        """ Calculates the l2 distance between two vectors """
-        return np.sqrt(np.sum((x1 - x2)**2, axis = 0))
+        return np.sqrt(np.sum((x1 - x2)**2))
 
 
 class KNN:
 
     def __init__(self, k=3):
         self.k = k
-        self.X_train = None
-        self.y_train = None
-
 
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
 
-
     def predict(self, X):
         y_pred = [self._predict(x) for x in X]
         return np.array(y_pred)
 
-
-    def _predict(self, X):
-            # Compute distances between x and all examples in the training set. 
-            # Sort by class and return only k neighbors
-            idx = np.argsort([euclidean_distance(X, x) for x in self.X_train])[:self.k]
-            # Extract the labels of the K nearest neighboring training samples
-            knn_targets = np.array([self.y_train[i] for i in idx])
-            
-            # Label sample as the most common class label
-            return self._vote(knn_targets)
-
-
-    def _vote(self, neighbor_labels):
-        """ Return the most common class among the neighbor samples """
-        most_common = Counter(neighbor_labels).most_common(1)
+    def _predict(self, x):
+        # Compute distances between x and all examples in the training set
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        # Sort by distance and return indices of the first k neighbors
+        k_idx = np.argsort(distances)[:self.k]
+        # Extract the labels of the k nearest neighbor training samples
+        k_neighbor_labels = [self.y_train[i] for i in k_idx]  
+        # return the most common class label
+        most_common = Counter(k_neighbor_labels).most_common(1)
         return most_common[0][0]
---
+++ mla/knn_tests.py
@@ -0,0 +1,36 @@
+import numpy as np
+from knn import KNN
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+# Inspect data
+
+#print(X_train.shape)
+#print(X_train[0])
+
+#print(y_train.shape)
+#print(y_train)
+
+#plt.figure()
+#plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap, edgecolor='k', s=20)
+#plt.show()
+
+k = 3
+clf = KNN(k=k)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+print("custom KNN classification accuracy", accuracy(y_test, predictions))
