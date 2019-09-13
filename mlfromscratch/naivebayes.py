68c047e74d5451b9fdf4242314314279ece32a11
Python Engineer <pythonengineerman@gmail.com>
2019-09-13 14:48:50 +0200

    added naive bayes

---
+++ mlfromscratch/naivebayes.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class NaiveBayes:
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        # calculate mean, var, and prior for each class
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors =  np.zeros(n_classes, dtype=np.float64)
+
+        for c in self._classes:
+            X_c = X[y==c]
+            self._mean[c, :] = X_c.mean(axis=0)
+            self._var[c, :] = X_c.var(axis=0)
+            self._priors[c] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        posteriors = []
+
+        # calculate posterior probability for each class
+        for idx, c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + posterior
+            posteriors.append(posterior)
+            
+        # return class with highest posterior probability
+        return self._classes[np.argmax(posteriors)]
+            
+
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(- (x-mean)**2 / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator
