c92eae9c13f6863ff9e3c9e95c62a8f33cc5a58a
janaSunrise <warriordefenderz@gmail.com>
2021-06-13 10:49:16 +0530

    Added testing for Regression file.

--- mlfromscratch/regression.py
+++ mlfromscratch/regression.py
@@ -56,3 +56,53 @@ class LogisticRegression(BaseRegression):
 
     def _sigmoid(self, x):
         return 1 / (np.exp(-x) + 1)
+
+
+# Testing
+if __name__ == "__main__":
+    # Imports
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
+
+    # Utils
+    def r2_score(y_true, y_pred):
+        corr_matrix = np.corrcoef(y_true, y_pred)
+        corr = corr_matrix[0, 1]
+        return corr ** 2
+
+    def mean_squared_error(y_true, y_pred):
+        return np.mean((y_true - y_pred) ** 2)
+
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+    # Linear Regression
+    X, y = datasets.make_regression(
+        n_samples=100, n_features=1, noise=20, random_state=4
+    )
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
+
+    regressor = LinearRegression(learning_rate=0.01, n_iters=1000)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+
+    accu = r2_score(y_test, predictions)
+    print("Linear reg Accuracy:", accu)
+
+    # Logistic reg
+    bc = datasets.load_breast_cancer()
+    X, y = bc.data, bc.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
+
+    regressor = LogisticRegression(learning_rate=0.0001, n_iters=1000)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+
+    print("LR classification accuracy:", accuracy(y_test, predictions))
